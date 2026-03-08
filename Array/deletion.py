85e2acc3f822c929f588cfd960b85d93336d76ab
hanifullah313 <hu4206835@gmail.com>
2026-03-08 21:14:00 +0500

    :

---
+++ Array/deletion.py
@@ -0,0 +1,15 @@
+# Python program to delete an element from the
+# beginning of a list using built-in functions
+
+arr = [10, 20, 30, 40]
+
+print("Array before deletion")
+for i in range(len(arr)):
+    print(arr[i], end=" ")
+
+# Remove the first element 
+del arr[0]
+
+print("\nArray after deletion")
+for i in range(len(arr)):
+    print(arr[i], end=" ")
